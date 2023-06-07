c2c3e25dde5a2b6ed1c0167c41e46df359deeeb1
Bea <rodri27b@mtholyoke.edu>
2023-06-07 17:47:56 +0200

    syntax tips implemented

--- multiprocessing_framework.py
+++ multiprocessing_framework.py
@@ -7,10 +7,15 @@ import os
 from pathlib import Path
 import sys
 
+LOGGING_LEVEL = logging.DEBUG
+LOGGING_FILE = 'surv_log.log'
+OUTPUT_FILE = 'mp_data_test.txt'
+NUM_OF_WORKERS = multiprocessing.cpu_count()
+
 #Configures the log
 def log_configurer():
     root = logging.getLogger()
-    h = logging.FileHandler('surv_log.log')
+    h = logging.FileHandler(LOGGING_FILE)
     f = logging.Formatter(
         '%(asctime)s %(processName)-10s %(name)s %(levelname)-8s %(message)s')
     h.setFormatter(f)
@@ -18,9 +23,9 @@ def log_configurer():
 
 # This is the listener process top-level loop: wait for logging events
 # (LogRecords)on the queue and handle them, done when None
-def log_queue_listener_process(queue, configurer,flag):
-    configurer()
-    while not flag.is_set():
+def log_writer(queue,fatal_flag):
+    log_configurer()
+    while not fatal_flag.is_set():
         try:
             record = queue.get()
             if record is None:
@@ -29,46 +34,43 @@ def log_queue_listener_process(queue, configurer,flag):
             logger.handle(record)
         except Exception:
             import traceback
-            print('Error in log_queue_listener_process:', file=sys.stderr)
+            print('Error in log_writer:', file=sys.stderr)
             traceback.print_exc(file=sys.stderr)
-            flag.set()
+            fatal_flag.set()
 
 # The worker configuration is done at the start of the worker process run.
-# Note that on Windows you can't rely on fork semantics, so each process
-# will run the logging configuration code when it starts.
 def worker_log_configurer(queue):
     h = logging.handlers.QueueHandler(queue)
     root = logging.getLogger()
-    if (root.hasHandlers()):
-        root.handlers.clear()
-    root.addHandler(h)
-    root.setLevel(logging.DEBUG)
+    if not (root.hasHandlers()):
+        root.addHandler(h)
+    root.setLevel(LOGGING_LEVEL) #try this log 1st, otherwise param
 
 # This is the listener process loop: wait for data writing events
 # on the queue and handle them, done when None.
-def data_queue_listener_process(data_queue, log_queue, configurer,flag):
-    configurer(log_queue)
-    with open(('mp_data_test.txt'), 'w') as f:
-        while not flag.is_set():
+def output_writer(data_queue, log_queue,fatal_flag):
+    worker_log_configurer(log_queue)
+    logger = logging.getLogger()
+    with open((OUTPUT_FILE), 'w') as f:
+        while not fatal_flag.is_set():
             try:
                 data = data_queue.get()
                 if data is None:
                     break
-                f.write(str(data))
-                f.flush()
-                logger = logging.getLogger()
-                logger.log(logging.DEBUG,f"Reporting Data from Queue")
+                if data is not None:
+                    f.write(str(data))
+                    logger.log(logging.DEBUG,f"Reporting Data from Queue")
             except Exception as e:
-                logger = logging.getLogger()
-                logger.log(logging.CRITICAL,f"Issue in data_queue_listener_process:\n{str(e)}")
-                flag.set()
+                logger.log(logging.CRITICAL,f"Issue in output_writer:\n{str(e)}")
+                fatal_flag.set()
+        f.flush()
 
 # Count the number lines and characters of each file.
 # Add this to data_queue.
-def analyze_file_process(filename, log_queue, configurer, data_queue):
-    configurer(log_queue)
+def analyze_file(filename, log_queue, data_queue):
+    worker_log_configurer(log_queue)
+    logger = logging.getLogger()
     try:
-        logger = logging.getLogger()
         logger.log(logging.DEBUG,f"Opening File: {filename}")
         with open(filename, 'r') as file:
             lines = file.readlines()
@@ -77,81 +79,76 @@ def analyze_file_process(filename, log_queue, configurer, data_queue):
         message = (f"File: {Path(filename).stem} | Lines: {line_count} | Characters: {char_count}\n")
         data_queue.put(message)
     except Exception as e:
-        logger = logging.getLogger()
         logger.log(logging.ERROR,
                 f"Error processing file: {filename} || FILE DATA NOT UPDATED\n{str(e)}")
 
 # Createe pool of workers. Watch files_queue generatd by finder.
 # When updated add task with new filepath to pool. Continue until None
-def file_queue_listener(file_queue, data_queue, log_queue, configurer, flag):
-    configurer(log_queue)
-    while not flag.is_set():
+def handle_file(file_queue, data_queue, log_queue, fatal_flag):
+    while not fatal_flag.is_set():
         try:
             file = file_queue.get()
             if file is None:
                 break
-            analyze_file_process(file, log_queue, worker_log_configurer, data_queue)
+            analyze_file(file, log_queue, data_queue)
         except Exception as e:
+            worker_log_configurer(log_queue)
             logger = logging.getLogger()
-            logger.log(logging.CRITICAL,f'Issue in file_queue_listener:\n{e}')
-            flag.set()
+            logger.log(logging.CRITICAL,f'Issue in handle_file:\n{e}')
+            fatal_flag.set()
 
 # Walk file directory, add file names to shared file queue. 
 # Send None when done.
-def file_finder_process(directory_path, file_queue, log_queue, configurer, flag):
-    configurer(log_queue)
+def find_file(directory_path, file_queue, log_queue, fatal_flag):
+    worker_log_configurer(log_queue)
+    logger = logging.getLogger()
     try:
         for root, _, filenames in os.walk(directory_path):
             for filename in filenames:
-                logger = logging.getLogger()
                 logger.log(logging.INFO,f"Found file: {filename}")
                 file_path = os.path.join(root, filename)
                 file_queue.put(file_path)
-        file_queue.put(None)
+        for _ in range(NUM_OF_WORKERS):
+            file_queue.put(None)
     except Exception as e:
-        logger = logging.getLogger()
-        logger.log(logging.CRITICAL,f'Issue in file_finder_process:\n{e}')
-        flag.set()
+        logger.log(logging.CRITICAL,f'Issue in find_file:\n{e}')
+        fatal_flag.set()
 
 # Create log, data, file queues. Create+start log, data,file listener processes.
-# Construct pool and apply file_queue_listener. 
+# Construct pool and apply handle_file. 
 # Send None when done to log and data queues to stop listeners.
 def main():
-     
-    open('surv_log.log', 'w').close() #clean logs each run
+    open(LOGGING_FILE, 'w').close() #clean logs each run
     if len(sys.argv) < 2:
-        directory_path = os.path.join('.git')
+        directory_path = os.path.join('test_data')
     else:
         directory_path = os.path.join(sys.argv[1]) #assume dir to scan is in the same location as this file
-    try:
-        if not os.path.exists(directory_path): 
-            raise Exception(f"Unable to find given directory: {directory_path}")
-        flag = multiprocessing.Manager().Event()
-        log_queue = multiprocessing.Manager().Queue(-1)
-        data_queue = multiprocessing.Manager().Queue(-1)
-        file_queue = multiprocessing.Manager().Queue(-1)
-        log_listener = multiprocessing.Process(target=log_queue_listener_process,
-                args=(log_queue, log_configurer,flag))
-        log_listener.start()
-        data_listener = multiprocessing.Process(target=data_queue_listener_process, 
-                args=(data_queue,log_queue, worker_log_configurer,flag))
-        data_listener.start()
-        finder = multiprocessing.Process(target=file_finder_process, 
-                args=(directory_path, file_queue, log_queue, worker_log_configurer,flag))
-        finder.start()
-        with multiprocessing.Pool() as pool:
-            pool.apply(file_queue_listener, 
-                args=(file_queue, data_queue, log_queue, worker_log_configurer, flag))
-        log_queue.put_nowait(None)
-        data_queue.put_nowait(None)
-        finder.join()
-        log_listener.join()
-        data_listener.join()
 
-    except Exception as e:
-        log_configurer()
-        logger = logging.getLogger()
-        logger.log(logging.CRITICAL,f"Issue in main:\n{str(e)}")
+    fatal_flag = multiprocessing.Manager().Event()
+
+    log_queue = multiprocessing.Manager().Queue(-1)
+    data_queue = multiprocessing.Manager().Queue(-1)
+    file_queue = multiprocessing.Manager().Queue(-1)
+    
+    log_writer_process = multiprocessing.Process(target=log_writer,
+            args=(log_queue,fatal_flag))
+    log_writer_process.start()
+    output_writer_process = multiprocessing.Process(target=output_writer, 
+            args=(data_queue,log_queue,fatal_flag))
+    output_writer_process.start()
+    finder = multiprocessing.Process(target=find_file, 
+            args=(directory_path, file_queue, log_queue,fatal_flag))
+    finder.start()
+    
+    with multiprocessing.Pool(NUM_OF_WORKERS) as pool:
+        results = pool.apply(handle_file, 
+            args=(file_queue, data_queue, log_queue, fatal_flag))
+
+    log_queue.put_nowait(None)
+    data_queue.put_nowait(None)
+
+    # log_queue.join()
+    # data_queue.join()
 
 if __name__ == '__main__':
     main()
